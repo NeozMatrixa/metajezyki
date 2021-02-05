b2d88440dcfb9c23e3eec704a768bf18a2b40d59
Radoslaw Jachimowicz <radjach@gmail.com>
2021-02-05 17:34:35 +0100

    Working page base 1

---
+++ src/components/ContactMe.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+        <p className="lead">Feel free to contact me for any work or suggestions</p>
+        <div className="row section-head">
+        
+          <div className="ten columns">
+            
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets">
+            
+          </aside>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,143 @@
+
+let resumeData = {
+  imagebaseurl: "https://gitlab.com/NeozMatrixa",
+  name: "Radosław Jachimowicz",
+  role: "Aspiring gameplay programmer",
+  linkedinId: "My ID",
+  skypeid: "My Skype",
+  roleDescription: " ",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.facebook.com/radek.jachimowicz.1",
+      className: "fa fa-facebook-square",
+    },
+    {
+      name: "github",
+      url: "https://gitlab.com/NeozMatrixa",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "https://neozmatrixa.itch.io/",
+      className: "fa fa-gamepad",
+    },
+  ],
+  aboutme:
+    "I am currently a student at Koszalin University of Technology. ",
+  address: "Poland",
+  website: "radjach@gmail.com",
+  education: [
+    {
+      UniversityName: "Koszalin University of Technology",
+      specialization: "Informatics",
+      MonthOfPassing: "Feb",
+      YearOfPassing: "2022",
+      Achievements: "",
+    },
+    {
+      UniversityName: "High School No III in Suwalki",
+      specialization: "Math and physics",
+      MonthOfPassing: "Jun",
+      YearOfPassing: "2018",
+      Achievements: "",
+    },
+    {
+      UniversityName: "State Music School in Suwałki",
+      specialization: "Accordion",
+      MonthOfPassing: "Jun",
+      YearOfPassing: "2018",
+      Achievements: "",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Polsystem 2004",
+      specialization: "Assistant Network Administrator",
+      MonthOfLeaving: "Jun",
+      YearOfLeaving: "2018",
+      Achievements: "",
+    },
+    {
+      CompanyName: "Cooperation with Omicron",
+      specialization: "Android App developer",
+      MonthOfLeaving: "Oct",
+      YearOfLeaving: "2019",
+      Achievements: "",
+    },
+  ],
+  skillsDescription: "I have a wide range of multimedia skills. In 2016, I completed a multimedia course in Krasnogruda, which significantly developed my knowledge in the field of photography and film editing. From 2017 I also started composing music and now I can play guitar, piano and accordion. At the same time, I was interested in computers all the time. I became interested in creating games in my second year of studies. 4th place in TK GameJam gave me the motivation to set up a department in the science club dealing with game development. Less than six months later, our club took 7th place in the Polish national game jam #ZOSTANWDOMUROBGRY. These events allowed me not only to improve my soft skills in the field of communication and team work, but also hard skills related to creating games.",
+  skills: [
+    {
+      skillname: "unity",
+      skillShowName: "Unity",
+    },
+    {
+      skillname: "csharp",
+      skillShowName: "C#",
+    },
+    {
+      skillname: "unreal",
+      skillShowName: "Unreal",
+    },
+    {
+      skillname: "cplusplus",
+      skillShowName: "C++",
+    },
+    {
+      skillname: "godot",
+      skillShowName: "Godot",
+    },
+    {
+      skillname: "python",
+      skillShowName: "Python",
+    },
+    {
+      skillname: "java",
+      skillShowName: "Java",
+    },
+    {
+      skillname: "sonyvegas",
+      skillShowName: "Sony Vegas",
+    },
+    {
+      skillname: "photoshop",
+      skillShowName: "Adobe Photoshop",
+    },
+    {
+      skillname: "flstudio",
+      skillShowName: "Fl Studio",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Agent ButterHands",
+      description: "Action-packed game where when you shoot, you lose your gun and then you need to find it to continue the mission!",
+      imgurl: "images/portfolio/i1.png",
+      url: "https://neozmatrixa.itch.io/agent-butterhands"
+    },
+    {
+      name: "Redesign Rebuild Reclaim",
+      description: "A short horror game about poem writer dealing with mental health issues.",
+      imgurl: "images/portfolio/i2.png",
+      url: "https://neozmatrixa.itch.io/rrr"
+    },
+    {
+      name: "Black & White",
+      description: "Simple platform game made in 4h",
+      imgurl: "images/portfolio/i3.png",
+      url: "https://neozmatrixa.itch.io/black-and-white"
+    },
+  ],
+  achievements: [
+    {
+      description: "",
+      name: "",
+    },
+    {
+      description: "",
+      name: "",
+    },
+  ],
+};
+export default resumeData;
